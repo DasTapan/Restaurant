5f551b1aae3afae735bde6a5ff54dae5b638ea1a
Das Tapan <tapands.das@gmail.com>
2022-11-03 12:42:56 +0530

    Add content to header

--- src/home.js
+++ src/home.js
@@ -1,12 +1,39 @@
-const pageLoad = () => {
-    const headerElement = document.createElement('div');
-    const mainElement = document.createElement('div');
-    const footerElement = document.createElement('div');
-    headerElement.classList.add('header');
-    mainElement.classList.add('main');
-    footerElement.classList.add('footer');
+const pageLoad = (element) => {
+    const _headerElement = document.createElement('div');
+    const _mainElement = document.createElement('div');
+    const _footerElement = document.createElement('div');
 
-    return { headerElement, mainElement, footerElement };
+    _headerElement.classList.add('header');
+    _mainElement.classList.add('main');
+    _footerElement.classList.add('footer');
+
+    const _headingSpan = document.createElement('span');
+    _headingSpan.classList.add('heading');
+    _headerElement.append(_headingSpan);
+
+    _headingSpan.textContent = 'Royal Restaurant';
+    _footerElement.textContent = '© DasTapan. All Rights Reserved';
+
+    const _navMenu = document.createElement('div');
+    _navMenu.classList.add('nav-menu');
+
+    const _homeLink = document.createElement('a');
+    _homeLink.textContent = 'Home';
+    const _menuLink = document.createElement('a');
+    _menuLink.textContent = 'Menu';
+    const _contactLink = document.createElement('a');
+    _contactLink.textContent = 'Contact';
+
+    _navMenu.append(_homeLink, _menuLink, _contactLink);
+    _headerElement.appendChild(_navMenu);
+
+    const _append = () => {
+        element.append(_headerElement, _mainElement, _footerElement);
+    }
+
+    const render = () => { _append() };
+
+    return { render };
 }
 
 export default pageLoad;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import pageLoad from './home.js';
 
 console.log('Jay Jagannath');
 const contentDiv = document.querySelector('.content');
-console.log(contentDiv);
-contentDiv.append(
-    pageLoad().headerElement,
-    pageLoad().mainElement,
-    pageLoad().footerElement
-);
+pageLoad(contentDiv).render();

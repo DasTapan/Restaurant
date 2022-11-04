f1fabf59ba8b0f667349345b5f1adaa252f8e875
Das Tapan <tapands.das@gmail.com>
2022-11-04 11:15:57 +0530

    Add tab switch logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './style.css';
 import pageLoad from './home.js';
 
-console.log('Jay Jagannath');
 const contentDiv = document.querySelector('.content');
 pageLoad(contentDiv).render();
 
-const mainDiv = document.querySelector('.main');
+let mainDiv = document.querySelector('.main');
 console.log(mainDiv);
+console.log(mainDiv.childElementCount);
 
 const handle = (event) => {
     let _linkId;
@@ -15,6 +15,48 @@ const handle = (event) => {
         _linkId = event.target.getAttribute('id');
     }
     console.log(_linkId);
+
+    switch (_linkId) {
+        case 'home-button':
+            wipe().doClean(mainDiv);
+            console.log('Everything cleared');
+            break;
+
+        case 'menu-button':
+            wipe().doClean(mainDiv);
+            console.log('Everything cleared');
+            for(let i=0; i<9; i++){
+                const element = document.createElement('div');
+                element.classList.add('menu-card');
+                mainDiv.appendChild(element);
+            }
+            break;
+
+        case 'contact-button':
+            wipe().doClean(mainDiv);
+            console.log('Everything cleared');
+            const element = document.createElement('div');
+            element.classList.add('contact-card');
+            mainDiv.appendChild(element);
+            break;
+
+        default:
+            break;
+    }
+};
+
+const wipe = () => {
+    const _clean = (element) => {
+        while (element.lastElementChild) {
+            element.removeChild(mainDiv.lastElementChild);
+        }
+    };
+
+    const doClean = (node) => {
+        _clean(node);
+    };
+
+    return { doClean };
 };
 
 contentDiv.addEventListener('click', handle);
